c46985fa51c28366ca55b1a43249ad0469d53c77
Magda Sz <pesahson@gmail.com>
2022-01-15 17:44:25 +0100

    rolling and holding dice

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import {useState} from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
+
+
+function App() {
+
+    const [dice, setDice] = useState(allNewDice())
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            })
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        setDice(allNewDice())
+    }
+
+    function holdDice(id) {
+          setDice((oldDice) =>
+            oldDice.map((die) => {
+              return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+            })
+          );
+    }
+    
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+    
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
+    )
+}
+
+export default App
